83af1512360a322f1dc3a985de14c62473522998
Natalia Tsareva <cornrow-copses0f@icloud.com>
2024-06-06 00:12:59 +0300

    ent User chema creation

---
+++ user-service/ent/schema/user.go
@@ -0,0 +1,35 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("user_id").Unique(),
+		field.String("username").NotEmpty(),
+		field.String("email").NotEmpty().Unique(),
+		field.Int("age").Positive(),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
+
+// Indexes of thev User
+func (User) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("user_id"),
+		index.Fields("email"),
+	}
+}
---
+++ user-service/go.mod
@@ -0,0 +1,5 @@
+module user-service
+
+go 1.22.1
+
+require entgo.io/ent v0.13.1
